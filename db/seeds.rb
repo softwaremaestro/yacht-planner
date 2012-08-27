5e8807c063caea430e44373de94362328b5caa0c
Jaeyoung <jaeyoung@monodiary.net>
2012-08-27 17:41:22 +0900

    insert dummy data into articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user2.name
-user.add_role :admin
+
+#puts 'SETTING UP DEFAULT USER LOGIN'
+#user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'New user created: ' << user.name
+#user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'New user created: ' << user2.name
+#user.add_role :admin
+
+puts "SETTING UP DEFAULT ARTICLE"
+article1 = Article.create! content: "content1", plan_id: "2", sub_category_id: "1", user_id: "1"
+article2 = Article.create! content: "content2", plan_id: "2", sub_category_id: "1", user_id: "1"
+article3 = Article.create! content: "content3", plan_id: "2", sub_category_id: "1", user_id: "1"
