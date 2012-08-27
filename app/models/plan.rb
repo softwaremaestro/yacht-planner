6faaac9d5dfedeb47c9a7d9e364103288be032a8
closer27 <closer527@gmail.com>
2012-08-27 21:50:34 +0900

    plans 페이지 레이아웃 일부 변경

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ActiveRecord::Base
-  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :cover, :styles => { :medium => "870x400>", :thumb => "300x200>" }
 
   validates :title, :presence => true
 end
