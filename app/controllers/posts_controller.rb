a6b50d0cab168703d62e9aec6e3fd1da810d36fd
chong <lee11326@gmail.com>
2012-08-29 00:13:57 +0900

    json_test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,29 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
+    @post = []
+    tmp = {}
+    tmp["image_url"]="https://encrypted-tbn0.google.com/images?q=tbn:ANd9GcRhpTOye5bnCu4L6I9tyx89jyIWBSCvb23dCHYvinHR2_3nPULfsw"
+    tmp["name"]="first"
+    tmp["content"]="asdadadd sadjsl akdjkasldsdl;jldasdljdlsmdnmas,nas si soduwq qw wle jlq qwj q; qwejl;;"
+
+    @post.push tmp
+
+    tmp = {}
+    tmp["image_url"]="http://c2thumb.cyworld.com/thumb_160x245/http://c2down.cyworld.co.kr/download?fid=6422369cb0938ea2a3a43841796ca3ee&name=mamas.jpg"
+    tmp["name"]="haha"
+    tmp["content"]="hoho"
+    @post.push tmp
+
+    render json: @post
+
+    #respond_to do |format|
+    #  format.html # show.html.erb
+    #  format.json { render json: @post }
+    #end
+
   end
 
   # GET /posts/new
