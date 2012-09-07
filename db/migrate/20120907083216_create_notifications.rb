9f91aae48279be13aa00ecbbe107f10739d6d832
chong <lee11326@gmail.com>
2012-09-07 20:08:29 +0900

    notification 만드는중

---
+++ db/migrate/20120907083216_create_notifications.rb
@@ -0,0 +1,14 @@
+class CreateNotifications < ActiveRecord::Migration
+  def change
+    create_table :notifications do |t|
+      t.integer :user_id
+      t.integer :plan_id
+      t.integer :sub_category_id
+      t.integer :article_id
+      t.integer :comment_id
+      t.integer :c_type
+      t.boolean :view
+      t.timestamps
+    end
+  end
+end
